9f04609881117d08bf930792f33a04b1c9d09556
jemgov <jesus.gomezvalero@outlook.es>
2025-10-24 18:33:21 +0200

    modificar cypress.config.js

--- mi-proyecto-cypress/cypress.config.js
+++ mi-proyecto-cypress/cypress.config.js
@@ -1,35 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/report",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveJson: true,
+    reportPageTitle: "Test-Suite"
+  },
   e2e: {
-    // Desactiva support file
     supportFile: false,
-    specPattern: "e2e/1-getting-started/PRUEBAS/*.cy.js", // <- esto es clave
-    // Configuración de eventos de Node
+    specPattern: "e2e/1-getting-started/PRUEBAS/*.cy.js",
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      // implementa aquí otros listeners si quieres
-      return config; // siempre devuelve config al final
+      return config;
     },
-
-    // Reportes con Mochawesome
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-      reportDir: "cypress/report",
-      charts: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveJson: true,
-      reportPageTitle: "Test-Suite",
-    },
-
-    // Otras configuraciones
     baseUrl: "https://example.cypress.io/",
     screenshotOnRunFailure: true,
     screenshotsFolder: "cypress/screenshots",
     video: true,
-    videosFolder: "cypress/video",
-    //videoCompression: true,
-    //trashAssetsBeforeRuns: true
-  },
+    videosFolder: "cypress/video"
+  }
 });
