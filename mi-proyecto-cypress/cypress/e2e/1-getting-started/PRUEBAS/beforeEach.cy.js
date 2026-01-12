b0264ca7c37226b32b14afee5480bfdf9ab4fc7d
jemgov <jesus.gomezvalero@outlook.es>
2026-01-12 18:59:58 +0100

    modificar pruebas

--- mi-proyecto-cypress/cypress/e2e/1-getting-started/PRUEBAS/beforeEach.cy.js
+++ mi-proyecto-cypress/cypress/e2e/1-getting-started/PRUEBAS/beforeEach.cy.js
@@ -6,13 +6,13 @@ describe("Prueba de beforeEach",() =>{      //beforeEach se ejecuta antes de cad
     it("navega a una url dinámica y valida el contenido",()=>{
         cy.visit("/commands/actions")
         cy.url().should("include","/commands/actions")
-        cy.get("h1").should("contain.text","Actions")
+        cy.get("h1").should("contain.text","Actionss")
         cy.wait(4000)
     })
     it("validar una segunda ruta",()=>{
         cy.get('.dropdown-toggle').click()
         cy.get('.dropdown-menu > :nth-child(2) > a').click()
         cy.url().should("include","/commands/traversal")
-        cy.get("h1").should("contain.text","Traversal")
+        cy.get("h1").should("contain.text","Traversall")
     })
 })
