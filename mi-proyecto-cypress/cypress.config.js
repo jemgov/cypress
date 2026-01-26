acab686c925a3fe5c0096f3e9357795c35b34a46
jemgov <jesus.gomezvalero@outlook.es>
2026-01-26 14:33:52 +0100

    actualizar cypress.config

--- mi-proyecto-cypress/cypress.config.js
+++ mi-proyecto-cypress/cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
   // ============================================================
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: "cypress/report/json",   // JSONs en carpeta estable
+    reportDir: "cypress/report/json",   // Carpeta estable
     saveJson: true,
     html: false,                 // NO generar HTML
     charts: false,
